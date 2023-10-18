f98d99a5dcffa6d0a46c2307f767ffe6e12d71c2
hintons1 <143753541+hintons1@users.noreply.github.com>
2023-10-18 01:35:07 +0000

    added users and login functionality

--- client/src/components/loginBadge.vue
+++ client/src/components/loginBadge.vue
@@ -1,35 +1,38 @@
 <script setup lang="ts">
-    import { getSession, login } from '@/model/session'
-    const session = getSession()
+  import { getSession, login, logout } from '@/components/model/session'
 
-    let user: any = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john@doe.com',
-    }
-    //user = null
+  const session = getSession()
+
+  const doLogin = () => {
+    login('hbingley1@plala.or.jp', 'CQutx25i8r')
+  }
+
+  const doLogout = () => {
+    logout();
+  }
+
 </script>
 
 <template>
-    <div class="has-text-right" v-if="session.user">
-        Welcome, {{ session.user.firstName }} {{ session.user.lastName }} <br>
-        <email>
-            {{ session.user.email }}
-            <a class="button is-small is-light is-warning" @click.prevent = "session.user = null">
-                <span class="icon">
-                    <i class="fas fa-sign-out-alt"></i>
-                </span>
-            </a>
-        </email>
-    </div>
-    <div class="buttons" v-else>
-        <a class="button is-primary">
-            <strong>Sign up</strong>
-        </a>
-        <a class="button is-light" @click.prevent="login">
-            Log in
-        </a>
-    </div>
+  <div class="has-text-right" v-if="session.user">
+    Welcome, {{ session.user.firstName }} {{ session.user.lastName }} <br>
+    <small>
+      {{ session.user.email }}
+      <a class="button is-small is-light is-warning" @click.prevent="doLogout">
+        <span class="icon">
+          <i class="fas fa-sign-out-alt"></i>
+        </span>
+      </a>
+    </small>
+  </div>
+  <div class="buttons" v-else>
+    <a class="button is-primary">
+      <strong>Sign up</strong>
+    </a>
+    <a class="button is-light" @click.prevent="doLogin">
+      Log in
+    </a>
+  </div>
 </template>
 
 
